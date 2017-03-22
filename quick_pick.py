3052a313e5eedd966f0a730afa7b2ca22d6ffc92
CB <conr.baird@my.jcu.edu.au>
2017-03-22 22:29:36 +1000

    Final Week 5

--- quick_pick.py
+++ quick_pick.py
@@ -1,11 +1,12 @@
 import random
-
 num_picks = int(input("How many quick picks?: "))
-numbers = random.randint(1,45)
-row = []
-count = 0
-
-for i in range(1, num_picks + 1):
-    while count < 6:
-        row.append(numbers)
+numbers = 6
+for i in range(num_picks):
+    row = []
+    count = 0
+    for i in range(numbers):
+        row.append(random.randint(1,45))
+        row = list(set(row))
+        row = sorted(row, key=int)
+    row = ' '.join(str(e) for e in row)
     print(row)
